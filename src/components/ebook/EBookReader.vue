871c369eefae22193f753d5b5f6b5612511a078a
yangxin <1393003255@qq.com>
2024-07-21 13:04:31 +0800

    feat: 阅读器翻页功能实现

--- src/components/ebook/EBookReader.vue
+++ src/components/ebook/EBookReader.vue
@@ -15,27 +15,57 @@ export default {
     ...mapGetters(['fileName'])
   },
   methods: {
+    prevPage () {
+      if (this.rendition) {
+        this.rendition.prev()
+      }
+    },
+    nextPage () {
+      if (this.rendition) {
+        this.rendition.next()
+      }
+    },
+    toggleTitleAndMenu () {
+
+    },
     initEpub () {
       const url = 'http://192.168.1.107:8081/' + this.fileName + '.epub'
-      this.book = new Epub(url)
+      this.book = new Epub(url, {
+        view: 'inline'
+      })
+
       this.rendition = this.book.renderTo('read', {
         width: innerWidth,
-        height: innerHeight,
-        method: 'default'
+        height: innerHeight
       })
       this.rendition.display()
-      // this.rendition.on('touchstart', event => {
-      //   console.log(event)
-      // })
-      // this.rendition.on('touchend', event => {
-      //   console.log(event)
-      // })
+      this.rendition.on('touchstart', event => {
+        this.touchStartX = event.changedTouches[0].clientX
+        this.touchStartTime = event.timeStamp
+      })
+      this.rendition.on('touchend', event => {
+        const offsetX = event.changedTouches[0].clientX - this.touchStartX
+        const time = event.timeStamp - this.touchStartTime
 
-      console.log(this.book)
+        if (time < 500 && offsetX > 40) {
+          this.prevPage()
+        } else if (time < 500 && offsetX < -40) {
+          this.nextPage()
+        } else {
+          this.toggleTitleAndMenu()
+        }
+
+        event.stopPropagation()
+      })
     }
   },
   mounted () {
-    const fileName = this.$route.params.fileName.split('|').join('/')
+    const fileNameParam = this.$route.params.fileName
+    if (!fileNameParam) {
+      return
+    }
+
+    const fileName = fileNameParam.split('|').join('/')
     this.$store.dispatch('setFileName', fileName)
       .then(() => {
         this.initEpub()
